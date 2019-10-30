57d4d371805282a27c1b6395b0f890d431ebedc6
Christopher Eckardt <ceckardt@office1.visual-meta.com>
2019-10-30 10:31:26 +0100

    adding SalesTracking Example and TechOps Toolbar

---
+++ assets/QM/SalesTracking.js
@@ -0,0 +1,24 @@
+<script type="text/javascript">
+    var vmt_pi = {
+        'trackingId': 'de-KZ0L0YXNAO',
+        'version' 	: 'a_0.0.1',
+        'type' 		: 'confirmed',
+        'amount'	: 20.49,
+        'skus' 		: ['12345-05-WHT-XL', '12345-05-WHT-L', '12345-05-WHT-S'],
+        'prices' 	: [5.49,10.00,5.00],
+        'currency'	: 'EUR'
+    };
+</script>
+
+<script type="text/javascript">
+    var vmt = {};
+    (function(d, p) {
+        var vmtr = d.createElement('script');
+        vmtr.type = 'text/javascript';
+        vmtr.async = true;
+        var cachebuster = Math.round(new Date().getTime() / 1000);
+        vmtr.src = ('https:' == p ? 'https' : 'http') + '://www.ladenzeile.de/controller/visualMetaTrackingJs?cb=' + cachebuster;
+        var s = d.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(vmtr, s);
+    })(document, document.location.protocol);
+</script>
